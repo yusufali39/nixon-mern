066e07b05e2389136b0fefffd6490ff39dea3cc1
Yusuf Ali <84458764+yusufali39@users.noreply.github.com>
2023-08-30 07:12:56 +0530

    adding faq

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ menu_btn.addEventListener('click', () => {
         h1Element.style.opacity = '0.1';
     }
 })
+heading = document.querySelector('.text');
+
+let text = 'hello world!!';
+let texts = ['Namastey.','Introducing nixon.']
+
+let i = 1;
+isForwards = true;
+textCount = 0;
+
+setInterval(function(){
+  heading.textContent = texts[textCount].substring(0,i);
+  if (isForwards){
+    i++;
+  }
+  else {
+    i--;
+  }
+  if(i > text.length + 10) {
+    isForwards = false;
+  }
+  if(i<0){
+    isForwards = true;
+    textCount++;
+  }
+  if(textCount > texts.length-1) {
+    textCount = 0;
+  }
+},100)
