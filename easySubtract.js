67537586092d9373811d23872503012b81e298ef
Yusuf Ali <84458764+yusufali39@users.noreply.github.com>
2023-09-02 10:52:28 +0530

    activating game navbar
    i adding a math game.

---
+++ easySubtract.js
@@ -0,0 +1,26 @@
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+const questionE = document.getElementById('question');
+const inputE = document.getElementById('input');
+const formE = document.getElementById('form');
+const scoreE = document.getElementById('score');
+let score = JSON.parse(localStorage.getItem('score'));
+if(!score){
+    score = 0;
+}
+scoreE.innerText = `Score: ${score}`;
+questionE.innerText = `What is ${num1} subtract by ${num2}?`;
+const correctAns = num1 - num2;
+formE.addEventListener('submit', ()=>{
+    const userAns = +inputE.value;
+    if(userAns === correctAns){
+        score++;
+        updateLocalStorage();
+    }else{
+        score--;
+        updateLocalStorage();
+    }
+});
+function updateLocalStorage(){
+    localStorage.setItem('score', JSON.stringify(score));
+}
